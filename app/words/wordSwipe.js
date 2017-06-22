8eb48a73079e96fd3110e333ddf3707a0dce52ae
Lorun <lobarev@vicman.net>
2017-06-22 13:39:06 +0300

    fix swipe during the scrolling

--- app/words/wordSwipe.js
+++ app/words/wordSwipe.js
@@ -11,7 +11,9 @@ export class WordSwipe extends Component {
         });
 
         this.start = {x: 0, y: 0};
+        this.end = {x: 0, y: 0};
         this.deltaX = 0;
+        this.deltaY = 0;
         this.controlsWidth = 0;
     }
 
@@ -45,6 +47,9 @@ export class WordSwipe extends Component {
         }
         this.deltaX = 0;
         this.start.x = 0;
+        this.start.y = 0;
+        this.end.x = 0;
+        this.end.y = 0;
         this.setState({isOpened: false});
     }
 
@@ -53,21 +58,37 @@ export class WordSwipe extends Component {
         if (!this.state.isOpened) {
             this.setState({isAnimated: false});
         }
-        this.start.x = this.start.x + ev.touches[0].clientX;
+        //this.start.x = this.start.x + ev.touches[0].clientX;
+        //this.start.y = this.start.y + ev.touches[0].clientY;
+        this.start.x = this.end.x = ev.touches[0].pageX;
+        this.start.y = this.end.y = ev.touches[0].pageY;
         this.controlsWidth = ev.currentTarget.children[1].offsetWidth;
+
     }
 
     handleTouchMove(ev) {
         let rightOffset;
 
-        this.deltaX = this.start.x - ev.touches[0].pageX;
+        this.end.x = ev.touches[0].pageX;
+        this.end.y = ev.touches[0].pageY;
+
+        this.deltaX = this.start.x - this.end.x;
         rightOffset = this.deltaX;
 
-        if (this.deltaX >= 0) {
-            if (this.deltaX > this.controlsWidth) {
-                rightOffset = this.controlsWidth + (this.deltaX - this.controlsWidth) * Math.tan(0.3);
+        /*if (rightOffset > 4) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'visible';
+        }*/
+
+        if (this.calculateDirection() === 'LEFT') {
+            ev.preventDefault();
+            if (this.deltaX >= 0) {
+                if (this.deltaX > this.controlsWidth) {
+                    rightOffset = this.controlsWidth + (this.deltaX - this.controlsWidth) * Math.tan(0.3);
+                }
+                ev.currentTarget.children[0].style.right = rightOffset + 'px';
             }
-            ev.currentTarget.children[0].style.right = rightOffset + 'px';
         }
     }
 
@@ -89,6 +110,40 @@ export class WordSwipe extends Component {
     handleButtonClick(callback) {
         this.closeControls();
         callback();
+    }
+
+
+
+    caluculateAngle() {
+        const X = this.start.x-this.end.x;
+        const Y = this.end.y-this.start.y;
+        const r = Math.atan2(Y,X); //radians
+        let angle = Math.round(r*180/Math.PI); //degrees
+
+        //ensure value is positive
+        if (angle < 0)
+            angle = 360 - Math.abs(angle);
+
+        return angle;
+    }
+
+    calculateDirection() {
+        let angle = this.caluculateAngle();
+
+        if ( (angle <= 45) && (angle >= 0) )
+            return 'LEFT';
+
+        else if ( (angle <= 360) && (angle >= 315) )
+            return 'LEFT';
+
+        else if ( (angle >= 135) && (angle <= 225) )
+            return 'RIGHT';
+
+        else if ( (angle > 45) && (angle < 135) )
+            return 'DOWN';
+
+        else
+            return 'UP';
     }
 
     render(props, state) {
